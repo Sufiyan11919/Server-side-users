b74f748cc43cce5c82f603d8d5a39e4e07ed827b
sufi <sufiyanshaikh11919@gmail.com>
2022-07-03 23:36:27 -0700

    created the handlebar file

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-
+const path = require('path');
+const exphbs = require('express-handlebars');
 const app = express();
-const port = process.env.PORT || 5000;
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+const port = process.env.PORT || 5002;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+
 
 // turn on routes
 app.use(routes);
 
-// turn on connection to db and server
+// turn on connection to db and serve
 sequelize.sync({ force: false }).then(() => {
   app.listen(port, () => console.log('Now listening'));
 });
