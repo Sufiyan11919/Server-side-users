26da104d395005fe8c6f5a0d5516cdbbfd5994f3
sufi <sufiyanshaikh11919@gmail.com>
2022-07-04 17:29:58 -0700

    log in/ sign up

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,10 +3,27 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
   res.render('homepage');
+  console.log(req.session);
 });
 
 router.get('/login', (req, res) => {
-    res.render('login');
-  });
+  if (!req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,8 +3,24 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
 const app = express();
 const hbs = exphbs.create({});
+
+const sess = {
+  secret: 'i am not a programmer',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
